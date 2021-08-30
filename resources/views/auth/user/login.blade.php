ffc9a2bc5c54757bf7794c15f35be57e4ca2e6de
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-08-30 10:31:43 +0700

    install Breeze

--- resources/views/auth/user/login.blade.php
+++ resources/views/auth/user/login.blade.php
@@ -2,8 +2,8 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-12 text-center">
-                <h2>Landing Page</h2>
+            <div class="col-12 text-center">
+                <button class="btn btn-primary mt-4">Sign In with Google</button>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('user-login', [UserController::class, 'login'])->name('user.login');
+
+require __DIR__.'/auth.php';

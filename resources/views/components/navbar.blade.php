d9c834ee949cd64377a50412096ed9560b025f50
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-09-17 14:12:15 +0700

    update landing page frontend

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,32 +1,46 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
-    <div class="container-fluid">
-    <a class="navbar-brand" href="{{route('welcome')}}">Laracamp</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{route('welcome')}}">Home</a>
-        </li>
-        @auth
-        <li class="nav-item">
-            <a class="nav-link" href="{{route('user.dashboard')}}">My Dashboard</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                Logout
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            </form>
-        </li>
-        @else
-        <li class="nav-item">
-            <a class="nav-link" href="{{route('login')}}">Login</a>
-        </li>
-        @endauth
-        </ul>
-    </div>
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <a class="navbar-brand" href="{{route('welcome')}}">
+            <img src="{{asset('images/logo.png')}}" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Program</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Mentor</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Business</a>
+                </li>
+            </ul>
+            <div class="d-flex">
+                @auth
+                    <a href="{{route('user.dashboard')}}" class="btn btn-master btn-secondary me-3">
+                        My Dashboard
+                    </a>
+                    <a class="btn btn-master btn-primary" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Sign Out
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    </form>
+                @else
+                    <a href="{{route('login')}}" class="btn btn-master btn-secondary me-3">
+                        Sign In
+                    </a>
+                    <a href="{{route('login')}}" class="btn btn-master btn-primary">
+                        Sign Up
+                    </a>
+                @endauth
+            </div>
+        </div>
     </div>
 </nav>

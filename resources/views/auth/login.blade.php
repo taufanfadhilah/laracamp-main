5a9a43727ba0a29061e605848a24b7defdbf89aa
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-09-19 18:33:15 +0700

    integrate new login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12 text-center">
-                <a href="{{route('user.login.google')}}" class="btn btn-primary mt-4">Sign In with Google</a>
-            </div>
+    <section class="login-user">
+        <div class="left">
+            <img src="{{asset('images/ill_login_new.png')}}" alt="">
         </div>
-    </div>
+        <div class="right">
+            <img src="{{asset('images/logo.png')}}" class="logo" alt="">
+            <h1 class="header-third">
+                Start Today
+            </h1>
+            <p class="subheader">
+                Because tomorrow become never
+            </p>
+            <p>
+                <a class="btn btn-border btn-google-login" href="{{route('user.login.google')}}">
+                    <img src="{{asset('images/ic_google.svg')}}" class="icon" alt=""> Sign In with Google
+                </a>
+            </p>
+            <img src="{{asset('images/people.png')}}" class="people" alt="">
+        </div>
+    </section>
 @endsection

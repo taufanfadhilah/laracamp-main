9450d996d53d6dda93463172e9bebe282d22213c
taufanfadhilah <taufanfadhilahiskandar@gmail.com>
2021-09-20 15:18:44 +0700

    integrate checkout page

--- resources/views/checkout/create.blade.php
+++ resources/views/checkout/create.blade.php
@@ -1,72 +1,90 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-6 offset-3">
-                <div class="card">
-                    <div class="card-header text-center">
-                        Checkout Bootcamp - {{$camp->title}}
-                    </div>
-                    <div class="card-body">
-                        <form action="{{route('checkout.store', $camp->id)}}" method="post">
-                            @csrf
-                            <div class="form-group mb-1">
-                                <label for="email">Email Address</label>
-                                <input name="email" type="email" class="form-control {{$errors->has('email') ? 'is-invalid' : ''}}" value="{{old('email') ?: Auth::user()->email}}" required />
-                                @if ($errors->has('email'))
-                                    <p class="text-danger">{{$errors->first('email')}}</p>
-                                @endif
+    <section class="checkout">
+        <div class="container">
+            <div class="row text-center pb-70">
+                <div class="col-lg-12 col-12 header-wrap">
+                    <p class="story">
+                        YOUR FUTURE CAREER
+                    </p>
+                    <h2 class="primary-header">
+                        Start Invest Today
+                    </h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-9 col-12">
+                    <div class="row">
+                        <div class="col-lg-5 col-12">
+                            <div class="item-bootcamp">
+                                <img src="{{asset('images/item_bootcamp.png')}}" alt="" class="cover">
+                                <h1 class="package text-uppercase">
+                                    {{$camp->title}}
+                                </h1>
+                                <p class="description">
+                                    Bootcamp ini akan mengajak Anda untuk belajar penuh mulai dari pengenalan dasar sampai membangun sebuah projek asli
+                                </p>
                             </div>
-                            <div class="form-group mb-1">
-                                <label for="name">Full Name</label>
-                                <input name="name" type="text" class="form-control {{$errors->has('name') ? 'is-invalid' : ''}}" value="{{Auth::user()->name}}" required />
-                                @if ($errors->has('name'))
-                                    <p class="text-danger">{{$errors->first('name')}}</p>
-                                @endif
-                            </div>
-                            <div class="form-group mb-1">
-                                <label for="occupation">Occupation</label>
-                                <input name="occupation" type="text" class="form-control {{$errors->has('occupation') ? 'is-invalid' : ''}}" value="{{old('occupation') ?: Auth::user()->occupation}}" required />
-                                @if ($errors->has('occupation'))
-                                    <p class="text-danger">{{$errors->first('occupation')}}</p>
-                                @endif
-                            </div>
-                            <div class="form-group mb-1">
-                                <label for="card_number">Card Number</label>
-                                <input name="card_number" type="text" class="form-control {{$errors->has('card_number') ? 'is-invalid' : ''}}" required />
-                                @if ($errors->has('card_number'))
-                                    <p class="text-danger">{{$errors->first('card_number')}}</p>
-                                @endif
-                            </div>
-                            <div class="row">
-                                <div class="col-6">
-                                    <div class="form-group">
-                                        <label for="expired">Expired</label>
-                                        <input name="expired" type="month" class="form-control {{$errors->has('expired') ? 'is-invalid' : ''}}" required />
-                                        @if ($errors->has('expired'))
-                                    <p class="text-danger">{{$errors->first('expired')}}</p>
-                                @endif
+                        </div>
+                        <div class="col-lg-1 col-12"></div>
+                        <div class="col-lg-6 col-12">
+                            <form action="{{route('checkout.store', $camp->id)}}" class="basic-form" method="post">
+                                @csrf
+                                <div class="mb-4">
+                                    <label for="email" class="form-label">Email Address</label>
+                                    <input name="email" type="email" class="form-control {{$errors->has('email') ? 'is-invalid' : ''}}" value="{{old('email') ?: Auth::user()->email}}" required />
+                                    @if ($errors->has('email'))
+                                        <p class="text-danger">{{$errors->first('email')}}</p>
+                                    @endif
+                                </div>
+                                <div class="mb-4">
+                                    <label for="name" class="form-label">Full Name</label>
+                                    <input name="name" type="text" class="form-control {{$errors->has('name') ? 'is-invalid' : ''}}" value="{{Auth::user()->name}}" required />
+                                    @if ($errors->has('name'))
+                                        <p class="text-danger">{{$errors->first('name')}}</p>
+                                    @endif
+                                </div>
+                                <div class="mb-4">
+                                    <label for="occupation" class="form-label">Occupation</label>
+                                    <input name="occupation" type="text" class="form-control {{$errors->has('occupation') ? 'is-invalid' : ''}}" value="{{old('occupation') ?: Auth::user()->occupation}}" required />
+                                    @if ($errors->has('occupation'))
+                                        <p class="text-danger">{{$errors->first('occupation')}}</p>
+                                    @endif
+                                </div>
+                                <div class="mb-4">
+                                    <label for="card_number" class="form-label">Card Number</label>
+                                    <input name="card_number" type="text" class="form-control {{$errors->has('card_number') ? 'is-invalid' : ''}}" required />
+                                    @if ($errors->has('card_number'))
+                                        <p class="text-danger">{{$errors->first('card_number')}}</p>
+                                    @endif
+                                </div>
+                                <div class="mb-5">
+                                    <div class="row">
+                                        <div class="col-lg-6 col-12">
+                                            <label for="expired" class="form-label">Expired</label>
+                                            <input name="expired" type="month" class="form-control {{$errors->has('expired') ? 'is-invalid' : ''}}" required />
+                                            @if ($errors->has('expired'))
+                                                <p class="text-danger">{{$errors->first('expired')}}</p>
+                                            @endif
+                                        </div>
+                                        <div class="col-lg-6 col-12">
+                                            <label for="cvc" class="form-label">CVC</label>
+                                            <input name="cvc" type="text" class="form-control {{$errors->has('cvc') ? 'is-invalid' : ''}}" maxlength="3" required />
+                                            @if ($errors->has('cvc'))
+                                              <p class="text-danger">{{$errors->first('cvc')}}</p>
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-6">
-                                    <div class="form-group">
-                                        <label for="cvc">CVC</label>
-                                        <input name="cvc" type="text" class="form-control {{$errors->has('cvc') ? 'is-invalid' : ''}}" maxlength="3" required />
-                                        @if ($errors->has('cvc'))
-                                    <p class="text-danger">{{$errors->first('cvc')}}</p>
-                                @endif
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row mt-3">
-                                <div class="d-grid gap-2">
-                                    <button class="btn btn-primary">Pay Now</button>
-                                </div>
-                            </div>
-                        </form>
+                                <button type="submit" class="w-100 btn btn-primary">Pay Now</button>
+                                <p class="text-center subheader mt-4">
+                                    <img src="{{asset('images/ic_secure.svg')}}" alt=""> Your payment is secure and encrypted.
+                                </p>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
